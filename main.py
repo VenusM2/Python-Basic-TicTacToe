8debfc1e08031487d3c5793de41617c0d23aa62c
Nawuto <44899592+ImNawuto@users.noreply.github.com>
2021-05-06 22:04:54 +0300

    Add files via upload
    Basic TicTacToe
    Version 1.2.0
    -----------------
    * Revamped X or O variables(now its a list)
    * Optimizations in loading
    * Added clicking sounds

--- main.py
+++ main.py
@@ -13,10 +13,15 @@ if you find any place that takes X as 2 or O as 1 in any variable report it to m
 
 import turtle
 import random
-
+import winsound
+import asyncio
 # previous winner to choose who will be the next first player
 rng = random.randint(1, 2)
-
+# list for the X or O variables
+xnum = ['xo1','xo2','xo3','xo4','xo5','xo6','xo7','xo8','xo9']
+b1color = ['orange','yellow','green','blue']
+b2color = ['yellow','green','blue','orange']
+b3color = ['green','blue','orange','yellow']
 # setup screen and play
 scrn = turtle.Screen()
 scrn.bgcolor("red")
@@ -35,56 +40,29 @@ playerbox.pensize(10)
 ystart = 70
 xstart = -70
 # set x and o turtles
-xo1 = turtle.Turtle()
-xo2 = turtle.Turtle()
-xo3 = turtle.Turtle()
-xo4 = turtle.Turtle()
-xo5 = turtle.Turtle()
-xo6 = turtle.Turtle()
-xo7 = turtle.Turtle()
-xo8 = turtle.Turtle()
-xo9 = turtle.Turtle()
-xo1.shape("square")
-xo2.shape("square")
-xo3.shape("square")
-xo4.shape("square")
-xo5.shape("square")
-xo6.shape("square")
-xo7.shape("square")
-xo8.shape("square")
-xo9.shape("square")
-xo1.shapesize(2)
-xo2.shapesize(2)
-xo3.shapesize(2)
-xo4.shapesize(2)
-xo5.shapesize(2)
-xo6.shapesize(2)
-xo7.shapesize(2)
-xo8.shapesize(2)
-xo9.shapesize(2)
-xo1.penup()
-xo2.penup()
-xo3.penup()
-xo4.penup()
-xo5.penup()
-xo6.penup()
-xo7.penup()
-xo8.penup()
-xo9.penup()
+for i in range(0, 9, 1):
+    xnum[i] = turtle.Turtle()
+    xnum[i].shape('square')
+    xnum[i].shapesize(2)
+    xnum[i].speed(0)
+    xnum[i].penup()
 # score variable setup
 xwinstat = 0
 owinstat = 0
 
 xstatplayer = turtle.Turtle()
 ostatplayer = turtle.Turtle()
+xstatplayer.speed(0)
+ostatplayer.speed(0)
 xstatplayer.penup()
 ostatplayer.penup()
-xstatplayer.goto(40, -175)
-ostatplayer.goto(40, -200)
+xstatplayer.goto(40, -180)
+ostatplayer.goto(40, -225)
 xstatplayer.hideturtle()
 ostatplayer.hideturtle()
 # Winner announcer
 wina = turtle.Turtle()
+wina.speed(0)
 wina.hideturtle()
 wina.penup()
 wina.setpos(40, 200)
@@ -110,6 +88,9 @@ replay.penup()
 replay.shape('retry.gif')
 replay.shapesize(0.2)
 replayable = False
+
+def RainbowColor(b1,b2,b3):
+    print('WIP not ready yet')
 # checks for win after every click on the board
 def WinCheck():
     global xo1C
@@ -133,42 +114,58 @@ def WinCheck():
         # horizontal line check for x
         if xo1C == 1 and xo2C == 1 and xo3C == 1:
             whowon = 1
+            RainbowColor(0,1,2)
         elif xo4C == 1 and xo5C == 1 and xo6C == 1:
             whowon = 1
+            RainbowColor(3,4,5)
         elif xo7C == 1 and xo8C == 1 and xo9C == 1:
             whowon = 1
+            RainbowColor(6,7,8)
         # curved lines check for x
         elif xo1C == 1 and xo5C == 1 and xo9C == 1:
             whowon = 1
+            RainbowColor(0,4,8)
         elif xo7C == 1 and xo5C == 1 and xo3C == 1:
             whowon = 1
+            RainbowColor(6,4,2)
         # vertical lines check for x
         elif xo1C == 1 and xo4C == 1 and xo7C == 1:
             whowon = 1
+            RainbowColor(0,3,6)
         elif xo2C == 1 and xo5C == 1 and xo8C == 1:
             whowon = 1
+            RainbowColor(1,4,7)
         elif xo3C == 1 and xo6C == 1 and xo9C == 1:
             whowon = 1
+            RainbowColor(2,5,8)
         # checks if O won in any way
         # horizontal lines check for o
         elif xo1C == 2 and xo2C == 2 and xo3C == 2:
             whowon = 2
+            RainbowColor(0,1,2)
         elif xo4C == 2 and xo5C == 2 and xo6C == 2:
             whowon = 2
+            RainbowColor(3,4,5)
         # curved lines check for o
         elif xo7C == 2 and xo8C == 2 and xo9C == 2:
             whowon = 2
+            RainbowColor(6,7,8)
         elif xo1C == 2 and xo5C == 2 and xo9C == 2:
             whowon = 2
+            RainbowColor(0,4,8)
         elif xo7C == 2 and xo5C == 2 and xo3C == 2:
             whowon = 2
+            RainbowColor(6,4,2)
         # vertical lines check for o
         elif xo1C == 2 and xo4C == 2 and xo7C == 2:
             whowon = 2
+            RainbowColor(0,3,6)
         elif xo2C == 2 and xo5C == 2 and xo8C == 2:
             whowon = 2
+            RainbowColor(1,4,7)
         elif xo3C == 2 and xo6C == 2 and xo9C == 2:
             whowon = 2
+            RainbowColor(2,5,8)
 
 
     if whowon != 0 and wasWon == False:
@@ -189,22 +186,26 @@ def WinCheck():
             print("X won")
             wasWon = True
             wina.clear()
-            wina.write("X is the WINNER", align='center',font=('arial',40,'normal'))
+            wina.write("X is the WINNER", align='center',font=('Comic Sans MS Bold', 40,'normal'))
             replay.write('Replay', align='center', font=('arial', 10, 'normal'))
             replay.showturtle()
             replay.goto(35, 135)
             replayable = True
             xwinstat += 1
+            xstatplayer.clear()
+            xstatplayer.write('X wins : ' + str(xwinstat), align='center', font=('Comic Sans MS Bold', 20, 'normal'))
         elif whowon == 2:
             print("O won")
             wasWon = True
             wina.clear()
-            wina.write("O is the WINNER", align='center', font=('arial', 40, 'normal'))
+            wina.write("O is the WINNER", align='center', font=('Comic Sans MS Bold', 40, 'normal'))
             replay.write('Replay', align='center')
             replay.showturtle()
             replay.goto(35, 135)
             replayable = True
             owinstat += 1
+            ostatplayer.clear()
+            ostatplayer.write('O wins : ' + str(owinstat), align='center', font=('Comic Sans MS Bold', 20, 'normal'))
 
     if xo1C != 0 and xo2C != 0 and xo3C != 0 and xo4C != 0 and xo5C != 0 and xo6C != 0 and xo7C != 0 and xo8C != 0 and xo9C != 0 and replayable == False:
         print("DRAW")
@@ -217,8 +218,18 @@ def WinCheck():
         replayable = True
 
 # after clicking the replay button or when you open the program the board "prepares" itself
+ostatplayer.write('O wins : ' + str(owinstat), align='center', font=('Comic Sans MS Bold', 20, 'normal'))
+xstatplayer.write('X wins : ' + str(owinstat), align='center', font=('Comic Sans MS Bold', 20, 'normal'))
+for item in range(3):
+    playerbox.sety(ystart)
+    playerbox.setx(-70)
+    for item in range(3):
+        for item in range(4):
+            playerbox.forward(70)
+            playerbox.right(90)
+        playerbox.forward(70)
+    ystart = ystart - 70
 def prboard():
-    global ystart
     global xo1C
     global xo2C
     global xo3C
@@ -229,7 +240,6 @@ def prboard():
     global xo8C
     global xo9C
     global replayable
-    global xstart
     global playerbox
     global whowon
     global wasWon
@@ -237,58 +247,32 @@ def prboard():
     global owinstat
     global isX
     global rng
-
-    xstatplayer.clear()
-    ostatplayer.clear()
-    xstatplayer.write('X wins : ' + str(xwinstat), align='center', font=('arial',15,'normal'))
-    ostatplayer.write('O wins : ' + str(owinstat), align='center', font=('arial',15,'normal'))
-
     wasWon = False
     whowon = 0
-    ystart = 70
-    xstart = -70
-
     # Is it X's turn or O's turn
     isX = False
     if rng == 2:
         isX = False
     if rng == 1:
         isX = True
-
     replayable = False
-    playerbox.clear()
     wina.clear()
     replay.clear()
     replay.hideturtle()
     replay.goto(35, 170)
-    for item in range(3):
-        playerbox.sety(ystart)
-        playerbox.setx(-70)
-        for item in range(3):
-            for item in range(4):
-                playerbox.forward(70)
-                playerbox.right(90)
-            playerbox.forward(70)
-        ystart = ystart - 70
 
-    xo1.shape("square")
-    xo2.shape("square")
-    xo3.shape("square")
-    xo4.shape("square")
-    xo5.shape("square")
-    xo6.shape("square")
-    xo7.shape("square")
-    xo8.shape("square")
-    xo9.shape("square")
-    xo1.setpos(-35,35)
-    xo2.setpos(35, 35)
-    xo3.setpos(105, 35)
-    xo4.setpos(-35, -35)
-    xo5.setpos(35, -35)
-    xo6.setpos(105, -35)
-    xo7.setpos(-35, -105)
-    xo8.setpos(35, -105)
-    xo9.setpos(105, -105)
+
+    for i in range(0, 9, 1):
+        xnum[i].shape('square')
+    xnum[0].setpos(-35,35)
+    xnum[1].setpos(35, 35)
+    xnum[2].setpos(105, 35)
+    xnum[3].setpos(-35, -35)
+    xnum[4].setpos(35, -35)
+    xnum[5].setpos(105, -35)
+    xnum[6].setpos(-35, -105)
+    xnum[7].setpos(35, -105)
+    xnum[8].setpos(105, -105)
     xo1C = 0
     xo2C = 0
     xo3C = 0
@@ -302,9 +286,9 @@ def prboard():
 
 
     if isX == True:
-        wina.write("X is the first", align='center',font=('arial',40,'normal'))
+        wina.write("X is the first", align='center',font=('Comic Sans MS Bold', 40,'normal'))
     if isX == False:
-        wina.write("O is the first", align='center',font=('arial',40,'normal'))
+        wina.write("O is the first", align='center',font=('Comic Sans MS Bold', 40,'normal'))
 
 # everytime you click the screen this method is called
 def onclickSquare(x, y):
@@ -320,95 +304,96 @@ def onclickSquare(x, y):
     global xo7C
     global xo8C
     global xo9C
+    winsound.PlaySound('ClickingSound.wav', winsound.SND_ASYNC)
     # x or o placement
     if x < -5 and x > -65 and y > 5 and y < 65:
         if xo1C == 0:
             if isX == True:
-                xo1.shape("x.gif")
+                xnum[0].shape("x.gif")
                 isX = False
                 xo1C = 1
             else:
-                xo1.shape("o.gif")
+                xnum[0].shape("o.gif")
                 isX = True
                 xo1C = 2
     elif x < 65 and x > 5 and y > 5 and y < 65:
         if xo2C == 0:
             if isX == True:
-                xo2.shape("x.gif")
+                xnum[1].shape("x.gif")
                 isX = False
                 xo2C = 1
             else:
-                xo2.shape("o.gif")
+                xnum[1].shape("o.gif")
                 isX = True
                 xo2C = 2
     elif x < 135 and x > 75 and y > 5 and y < 65:
         if xo3C == 0:
             if isX == True:
-                xo3.shape("x.gif")
+                xnum[2].shape("x.gif")
                 isX = False
                 xo3C = 1
             else:
-                xo3.shape("o.gif")
+                xnum[2].shape("o.gif")
                 isX = True
                 xo3C = 2
     elif x < -5 and x > -65 and y > -65 and y < -5:
         if xo4C == 0:
             if isX == True:
-                xo4.shape("x.gif")
+                xnum[3].shape("x.gif")
                 isX = False
                 xo4C = 1
             else:
-                xo4.shape("o.gif")
+                xnum[3].shape("o.gif")
                 isX = True
                 xo4C = 2
     elif x < 65 and x > 5 and y > -65 and y < -5:
         if xo5C == 0:
             if isX == True:
-                xo5.shape("x.gif")
+                xnum[4].shape("x.gif")
                 isX = False
                 xo5C = 1
             else:
-                xo5.shape("o.gif")
+                xnum[4].shape("o.gif")
                 isX = True
                 xo5C = 2
     elif x < 135 and x > 75 and y > -65 and y < -5:
         if xo6C == 0:
             if isX == True:
-                xo6.shape("x.gif")
+                xnum[5].shape("x.gif")
                 isX = False
                 xo6C = 1
             else:
-                xo6.shape("o.gif")
+                xnum[5].shape("o.gif")
                 isX = True
                 xo6C = 2
     elif x < -5 and x > -65 and y > -135 and y < -75:
         if xo7C == 0:
             if isX == True:
-                xo7.shape("x.gif")
+                xnum[6].shape("x.gif")
                 isX = False
                 xo7C = 1
             else:
-                xo7.shape("o.gif")
+                xnum[6].shape("o.gif")
                 isX = True
                 xo7C = 2
     elif x < 65 and x > 5 and y > -135 and y < -75:
         if xo8C == 0:
             if isX == True:
-                xo8.shape("x.gif")
+                xnum[7].shape("x.gif")
                 isX = False
                 xo8C = 1
             else:
-                xo8.shape("o.gif")
+                xnum[7].shape("o.gif")
                 isX = True
                 xo8C = 2
     elif x < 135 and x > 75 and y > -135 and y < -75:
         if xo9C == 0:
             if isX == True:
-                xo9.shape("x.gif")
+                xnum[8].shape("x.gif")
                 isX = False
                 xo9C = 1
             else:
-                xo9.shape("o.gif")
+                xnum[8].shape("o.gif")
                 isX = True
                 xo9C = 2
 
@@ -422,6 +407,7 @@ def onclickSquare(x, y):
 
 # board preperation on first start
 prboard()
+
 
 # on click function
 
